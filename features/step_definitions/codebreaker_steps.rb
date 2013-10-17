e03df5ece2b09bfd58d8254552368e4aa7867deb
Jonathan Worek <wojobucco@gmail.com>
2013-10-17 12:10:56 -0400

    Through chapter 5 in book

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -2,7 +2,7 @@ Given(/^I am not yet playing$/) do
 end
 
 When(/^I start a new game$/) do
-  CodeBreaker::Game.new(output).start
+  Codebreaker::Game.new(output).start
 end
 
 Then(/^I should see "(.*?)"$/) do |message|

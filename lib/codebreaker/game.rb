1e1048ebd99139814dc39e3d539bdbc8c38597c0
Jonathan Worek <wojobucco@gmail.com>
2013-10-17 14:36:48 -0400

    got the basic one number match case working

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -5,11 +5,19 @@ module Codebreaker
     end
 
     def start(secret)
+      @secret = secret
       @output.puts("Welcome to Codebreaker!")
       @output.puts("Enter guess:")
     end
 
     def guess(guess)
+      if @secret.include?(guess[0])
+        mark = '-'
+      else
+        mark = ''
+      end
+
+      @output.puts mark
     end
   end
 end

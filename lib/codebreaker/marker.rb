3c60410b5b0704a364fe706d98941698e24d3a3d
Jonathan Worek <wojobucco@gmail.com>
2013-10-17 22:33:04 -0400

    Finished chapter 8.  Everything works now

--- lib/codebreaker/marker.rb
+++ lib/codebreaker/marker.rb
@@ -12,15 +12,24 @@ module Codebreaker
     end
 
     def number_match_count
-      (0..3).inject(0) do |count, index|
-        count + (number_match?(index) ? 1 : 0)
-      end
+      total_match_count - exact_match_count
     end
 
     private
 
+    def total_match_count
+      secret = @secret.split('')
+      @guess.split('').inject(0) do |count, n|
+        count + (delete_first(secret, n) ? 1: 0)
+      end
+    end
+
+    def delete_first(code, n)
+      code.delete_at(code.index(n)) if code.index(n)
+    end
+
     def exact_match?(index)
-      @guess[index] == @secret[index]
+      result = @guess[index] == @secret[index]
     end
     
     def number_match?(index)    
